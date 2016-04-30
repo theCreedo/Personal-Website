cfcd4c23ece7ba8450aff79f3f7373cdf9f7f4a8
Eric Lee <hownl96@utexas.edu>
2016-04-30 12:47:20 -0500

    Update projects.html - Add a paragraph between the buttons to describe the picture. myScript.js - Add innerHTML text change for paragraph under the image

--- myScript.js
+++ myScript.js
@@ -1,19 +1,25 @@
 function previousSnail() {
         var image = document.getElementById("Snail");
+	var imgtext = document.getElementById("imgT");
         if(image.src.match("Main")) {
                 image.src = "Gameplay3.png";
+		imgtext.innerHTML = " - 5 out of 5 : Gameplay 3 - ";
         }
         else if(image.src.match("play3")) {
                 image.src = "Gameplay2.png";
+		imgtext.innerHTML = " - 4 out of 5 : Gameplay 2 - ";
         }
         else if(image.src.match("play2")) {
                 image.src = "Gameplay1.png";
+		imgtext.innerHTML = " - 3 out of 5 : Gameplay 1 - ";
         }
         else if(image.src.match("play1")) {
                 image.src = "Story.png";
+		imgtext.innerHTML = " - 2 out of 5 : Story Line - ";
         }
         else if(image.src.match("Story")) {
                 image.src = "MainMenu.png";
+		imgtext.innerHTML = " - 1 out of 5 : Main Menu - ";
         }
 
 }
@@ -22,18 +28,23 @@ function nextSnail() {
 	var image = document.getElementById("Snail");
 	if(image.src.match("Main")) {
 		image.src = "Story.png";
+		imgtext.innerHTML = " - 2 out of 5 : Story Line - ";
 	}
 	else if(image.src.match("Story")) {
 		image.src = "Gameplay1.png";
+		imgtext.innerHTML = " - 3 out of 5 : Gameplay 1 - ";
 	}
         else if(image.src.match("play1")) {
                 image.src = "Gameplay2.png";
+		imgtext.innerHTML = " - 4 out of 5 : Gameplay 2 - ";
         }
         else if(image.src.match("play2")) {
                 image.src = "Gameplay3.png";
+		imgtext.innerHTML = " - 5 out of 5 : Gameplay 3 - ";
         }
         else if(image.src.match("play3")) {
                 image.src = "MainMenu.png";
+		imgtext.innerHTML = " - 1 out of 5 : Main Menu - ";
         }
 
 }
